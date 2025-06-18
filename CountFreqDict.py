0977ff1673d0a5d31b6b36f3c525b0936bceb6bb
Neeraj <166809692+neeraejr@users.noreply.github.com>
2025-06-18 19:49:58 +0530

    Delete CountFreqDict.py

--- CountFreqDict.py
+++
@@ -1,6 +0,0 @@
-# 🧩 Problem: Count frequency of each character in a string using dictionary (without collections).
-st = input("Enter String: ")
-d = {}
-for w in set(st):
-    d[w] = st.count(w)
-print(d)
